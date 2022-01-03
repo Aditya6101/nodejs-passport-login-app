758505a7df14216c1c2a3a31239545aa7054e2aa
Aditya Kamble <adityakamble6101@gmail.com>
2022-01-03 17:04:05 +0530

    feat: registration of the user.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,9 +15,32 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
+
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 // Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User Model
+const User = require('../models/User');
 
 // Login page
 router.get('/login', (req, res) => res.render('login'));
 
 // Register page
 router.get('/register', (req, res) => res.render('register'));
+
+// Handle Register
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  let errors = [];
+
+  // Check for required fields
+  if (!name || !email || !password || !confirmPassword)
+    errors.push({ msg: 'Please fill in all fields' });
+
+  // Check for password match
+  if (password !== confirmPassword)
+    errors.push({ msg: 'Passwords do not match' });
+
+  //   Check for password length
+  if (password.length < 6)
+    errors.push({ msg: 'Password must be at least 6 characters' });
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  } else {
+    // Check if user already exists
+    User.findOne({ email: email }).then((user) => {
+      // If user already exists
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword,
+        });
+      }
+      // If user not exists
+      else {
+        // Create new user
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+
+        // Hash Password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            // Set password to hashed password
+            newUser.password = hash;
+            // Save user to database
+            newUser.save().then((user) => {
+              req.flash('success_msg', 'You are now registered and can log in');
+              console.log(newUser);
+              res.redirect('/users/login');
+            });
+          })
+        );
+      }
+    });
+  }
+});
 
 module.exports = router;
