b956c7616a6bce208ddc00b17aad5e5af74a4299
Aditya Kamble <adityakamble6101@gmail.com>
2022-01-03 12:15:56 +0530

    new changes.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
+
+// DB configs
+const db = require('./config/keys').MongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, console.log(`Server running on Port: ${PORT}`));
